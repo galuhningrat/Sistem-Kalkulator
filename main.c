d90ad687f37af0c953fc0797085bd4dab504985f
galuhningrat <147510855+galuhningrat@users.noreply.github.com>
2024-06-04 05:31:29 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+/* buat sistem kalkulator. gunakan dan gabungkan fungsi if else bersarang,
+ * for loop, while do, switch. Buat sistem sesimpel mugkin tapi bisa
+ * mengakomodir selutuh sistrem. */
+
+#include <stdio.h>
+int main()
+{
+    int option;
+    float a, b, result;
+    printf("\nMasukkan bilangan pertama :");
+    scanf("%f", &a);
+
+    printf("\n\tMENU\n");
+    printf("1. Penjumlahan\n");
+    printf("2. Pengurangan\n");
+    printf("3. Perkalian\n");
+    printf("4. Pembagian\n");
+    printf("Pilih operasi: \n");
+    scanf("%d", &option);
+
+    printf("\nMasukkan bilangan kedua :");
+    scanf("%f", &b);
+
+
+    if(option==1) {
+        result = a + b;
+        printf("\n%g + %g = %g\n ", a, b, result);
+    }
+    else if(option==2) {
+        result = a - b;
+        printf("\n%g - %g = %g\n ", a, b, result);
+
+    } else if(option==3) {
+        result = a * b;
+        printf("\n%g * %g = %g\n ", a, b, result );
+
+    }else if(option==4){
+        result = a / b;
+        printf("\n%g / %g = %g\n ", a, b, result);
+
+    }else{
+        printf("Pilihan operasi tidak valid");
+    }
+}
